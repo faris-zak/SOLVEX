1e5822b1fd6b7570ae5696fef9ad395838a9f78c
Al-Faris Mujahid AlZakwani <a.alzakwani@outlook.com>
2025-12-06 12:53:19 +0400

    Redesign navbar and contact section for mobile
    Added a responsive dropdown menu for the navbar with improved mobile support, updated styles for scrollbars and color scheme, and replaced the contact section with a logo image. Updated JavaScript to handle the new dropdown menu logic and improved accessibility with titles and click handlers. Footer icons were updated to use Font Awesome.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,48 +1,37 @@
 console.log("Connected Successfully.");
 
-const dropDownBtn = document.querySelector('.dropBtn');
+const dropDownBtn = document.querySelector('.slideMenu');
 const dropdownContent = document.querySelector('.dropdownContent');
 
 
-dropDownBtn.addEventListener('change', function(){
+dropDownBtn.addEventListener('click', function(){
   if(dropdownContent.style.opacity == 1 && dropdownContent.style.zIndex == 1){
         dropdownContent.style.zIndex = -1;
         dropdownContent.style.opacity = 0;
+        dropdownContent.style.display = 'none';
         dropdownContent.style.transform = 'translateY(0)';
         dropdownContent.style.userSelect = 'none';
   } else{
         dropdownContent.style.zIndex = 1;
         dropdownContent.style.opacity = 1;
+        dropdownContent.style.display = 'block';
         dropdownContent.style.transform = 'translateY(10px)';
         dropdownContent.style.userSelect = 'auto';
   }
 });
-
-
-var myButton = document.getElementById("toTop");
-
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    myButton.style.opacity = "1";
-  } else {
-    myButton.style.opacity = "0";
-  }
-}
 
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
 
-
-  document.addEventListener('click', (event) => {
-    if (!dropDownBtn.contains(event.target) && !dropdownContent.contains(event.target)) {
+document.addEventListener('click', function(event) {
+  const isClickInside = dropDownBtn.contains(event.target) || dropdownContent.contains(event.target);
+  if (!isClickInside) {
         dropdownContent.style.zIndex = -1;
         dropdownContent.style.opacity = 0;
+        dropdownContent.style.display = 'none';
         dropdownContent.style.transform = 'translateY(0)';
         dropdownContent.style.userSelect = 'none';
-      dropDownBtn.checked = false;
-    }
-  });
+  }
+});
